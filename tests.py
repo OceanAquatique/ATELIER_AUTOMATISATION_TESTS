9fe5949c23fe43e3bb5baafb891cac624dca1093
OceanAquatique <oceanegoncalves00@gmail.com>
2026-03-11 15:35:23 +0100

    Delete tests.py

--- tests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://coffee.alexflipnote.dev/random.json"
-
-response = requests.get(url)
-
-print("Status code :", response.status_code)
-print("Content-Type :", response.headers.get("Content-Type"))
-print("JSON :", response.json())
